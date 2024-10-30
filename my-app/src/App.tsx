3c4706c3aa9bfe7e4569e10c927c63dd25e2d0d8
Julien Desmangles <89116447+ItsDezi@users.noreply.github.com>
2024-10-30 01:02:52 -0400

    file organization and started navbar

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './styles/App.css';
+import { Router, Routes, Route,BrowserRouter } from 'react-router-dom';
+import Home from './Pages/Home';
+import Navbar from './Components/NavBar';
 function App() {
   return (
+    <BrowserRouter>
+    <Navbar />
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+          </Routes>
       </header>
     </div>
+    </BrowserRouter>
+
   );
 }
 
---
+++ my-app/src/Components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '../styles/navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        
+    </nav>
+  );
+};
+
+export default Navbar;
