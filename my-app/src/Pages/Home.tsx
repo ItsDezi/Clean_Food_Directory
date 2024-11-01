15466d8d64f1ad43c1688faadc72f377ad57e9a8
Julien Desmangles <89116447+ItsDezi@users.noreply.github.com>
2024-11-01 18:26:08 -0400

    Home page changes

--- my-app/src/Pages/Home.tsx
+++ my-app/src/Pages/Home.tsx
@@ -2,8 +2,32 @@ import Background from '../Assets/foodBackground.png';
 
 function Home() {
     return (
-      <div className="App">
-        <img src={Background} />
+      <div className="App Home-background">
+        <div className='landing-page-slogan'>
+          <div className='wrapper-ting'>
+
+          <span>
+          <h1>
+          Find what's good in your hood.
+        </h1>
+          </span>
+        <span>
+        <h3>
+          Welcome to our community led clean food directory.
+        </h3>
+        </span>
+
+        <h5>
+          <span>Whether you're here to share your spots... </span>
+        </h5>
+        <h5>
+        <span>or to find some new ones, we've got you covered.</span>
+        </h5>
+        </div>
+
+        </div>
+
+
       </div>
     );
   }
