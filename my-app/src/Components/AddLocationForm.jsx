f8968f176ad82a47f0e48696668d130af4075d41
Julien Desmangles <89116447+ItsDezi@users.noreply.github.com>
2024-11-19 18:14:51 -0500

    added form fields

--- my-app/src/Components/AddLocationForm.jsx
+++ my-app/src/Components/AddLocationForm.jsx
@@ -44,9 +44,64 @@ function AddLocationForm() {
             (i.e. Eggs in Boston)
           </Form.Text>
         </Form.Group>
+
         <Form.Group>
           <Form.Label>Search for the location or find it on the map if there is no official address.</Form.Label>
-        <TabComponents />
+        <TabComponents/>
+        </Form.Group>
+
+        <Form.Group className='py-4'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control id='email' type="email" placeholder="marthasEggs@organic.com" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control id='phoneNumber' type="phone" placeholder="123-456-7890" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Description</Form.Label>
+          <Form.Control id='description' type="textarea" as="textarea" placeholder="" />
+        </Form.Group>
+
+        <Form.Group className='form-checkbox'>
+          <Form.Check label="Parking available" id='parkingAvailable' type="checkbox" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Website</Form.Label>
+          <Form.Control id='websiteURL' type="URL" placeholder="www.marthaeggs.com" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Instagram</Form.Label>
+          <Form.Control id='instagramHandle' type="URL" placeholder="@eggs_by_martha" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Facebook</Form.Label>
+          <Form.Control id='facebookURL' type="URL" placeholder="www.facebook.com/eggs_by_martha" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Youtube</Form.Label>
+          <Form.Control id='youtubeLink' type="URL" placeholder="www.youtube.com/eggs_by_martha" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Twitter</Form.Label>
+          <Form.Control id='twitterLink' type="URL" placeholder="@eggTweets" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Tik Tok</Form.Label>
+          <Form.Control id='tiktokLink' type="URL" placeholder="@marthasTikToks" />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Additional Links</Form.Label>
+          <Form.Control id='additionalLinks' type="URL" placeholder="www.yelp.com/eggs_by_martha" />
         </Form.Group>
       </Form>
     </div>
