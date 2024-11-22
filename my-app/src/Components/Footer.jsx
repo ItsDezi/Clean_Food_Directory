62971fc03065f60d33b3c4d5db2b40fa5450e5c7
Julien Desmangles <89116447+ItsDezi@users.noreply.github.com>
2024-11-21 23:58:45 -0500

    form changes and footer mods

--- my-app/src/Components/Footer.jsx
+++ my-app/src/Components/Footer.jsx
@@ -4,11 +4,15 @@ import { APP_TITLE } from '../Assets/constants';
 function Footer() {
     return (
         <>
-<footer className="background-color: #f8f9fa; padding: 20px; text-align: center; font-size: 14px; color: #6c757d;">
-  <div>
+<footer className="background-color: var(--primary); padding: 20px; text-align: center; font-size: 14px; color: #6c757d;">
+    <div className='footer-separator'></div>
+    <div>
+  <div className='left-footer'>
     <h2>{APP_TITLE}</h2>
     <p>Empowering your journey to a healthier lifestyle.</p>
   </div>
+  </div>
+  <div className='right-footer'>
   <div className="margin: 20px 0;">
   <ul className='footer-menu'>
             <li>
@@ -27,6 +31,7 @@ function Footer() {
   </div>
   <div>
     {/* <p>Contact us: <a href="mailto:support@example.com" className="color: #007bff;">support@example.com</a></p> */}
+  </div>
   </div>
   <div>
     <p>© 2024 {APP_TITLE}. All rights reserved.</p>
