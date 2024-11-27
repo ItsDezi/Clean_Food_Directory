61d3bd8e4147ded72af4fd1fe0e03710122002f8
ItsDezi <j_desman@live.concordia.ca>
2024-11-27 18:50:51 -0500

    icon formatting

--- my-app/src/Pages/ListingDetails.jsx
+++ my-app/src/Pages/ListingDetails.jsx
@@ -3,6 +3,10 @@ import { useEffect, useState } from 'react';
 import { getLocationById } from '../Services/apiService';
 import { useParams } from 'react-router-dom';
 import { template } from '../formDatatemplate';
+import { FaInstagram, FaFacebookSquare, FaYoutubeSquare, FaLink } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { AiFillTikTok } from "react-icons/ai";
+
 import {
   MapContainer,
   TileLayer,
@@ -13,6 +17,7 @@ import {
 import L from "leaflet";
 import icon from '../Assets/clean_food_directory_logo.png';
 function ListingDetails({ data }) {
+  const iconSize = {size:40};
   const {markerId} = useParams();
   const [loading, setLoading] = useState(false);
   const [ details, setDetails ] = useState(template.location);// = template.location;
@@ -32,6 +37,10 @@ function ListingDetails({ data }) {
         iconUrl: icon,
         iconSize: [75, 75]
       });
+      function formatDate(date) {
+        const index = date.indexOf('T');
+        return date.substring(0, index);
+      }
   return (
     <div className="details-container-container">
     {loading ? (
@@ -39,8 +48,10 @@ function ListingDetails({ data }) {
 
     ) : (
     <div className="details-container" style={{ padding: '20px', maxWidth: '800px', margin: 'auto', border: '1px solid #ccc', borderRadius: '8px' }}>
+      <h1>{details.name}</h1>
       <MapContainer
         className="map-container-contribute"
+        style={{width:'100%'}}
         center={[details.latitude, details.longitude]}
         minZoom={2}
         zoom={12}
@@ -58,24 +69,23 @@ function ListingDetails({ data }) {
 
         <Marker position={[details.latitude, details.longitude]} interactive={false} icon={customIcon}/>
       </MapContainer>
-      <h1>{details.name}</h1>
       <span><strong>Description: </strong> {details.description}</span>
       <span><strong>Address: </strong>{details.address}, {details.city}, {details.state}, {details.postalcode}, {details.country}</span>
       <span><strong>Coordinates: </strong>{details.latitude}, {details.longitude}</span>
       <span><strong>Email: </strong><a className="color:black" href={`mailto:${details.email}`}>{details.email}</a> </span> 
       <span><strong>Phone Number: </strong>{details.phoneNumber}</span>
       <span><strong>Parking Available: </strong>{details.parkingAvailable ? 'Yes' : 'No'}</span>
-      <span><strong>Notes: </strong>{details.notes}</span>
-      <span><strong>Last Updated: </strong>{details.lastUpdated}</span>
+      {/* <span><strong>Notes: </strong>{details.notes}</span> */}
+      <span><strong>Last Updated: </strong>{formatDate(details.lastUpdated)}</span>
       <div>
         <h3>Links</h3>
-        <ul>
-          <li><a href={details.websiteURL} target="_blank" rel="noopener noreferrer">Website</a></li>
-          <li><a href={details.facebookURL} target="_blank" rel="noopener noreferrer">Facebook</a></li>
-          <li><a href={details.twitterLink} target="_blank" rel="noopener noreferrer">Twitter</a></li>
-          <li><a href={details.instagramHandle} target="_blank" rel="noopener noreferrer">Instagram</a></li>
-          <li><a href={details.youtubeLink} target="_blank" rel="noopener noreferrer">YouTube</a></li>
-          <li><a href={details.tiktokLink} target="_blank" rel="noopener noreferrer">TikTok</a></li>
+        <ul style={{display:'inline-flex', }}>
+          <li><a href={details.websiteURL} target="_blank" rel="noopener noreferrer"><FaLink size={iconSize.size}/></a></li>
+          <li><a href={details.facebookURL} target="_blank" rel="noopener noreferrer"><FaFacebookSquare size={iconSize.size}/></a></li>
+          <li><a href={details.twitterLink} target="_blank" rel="noopener noreferrer"><FaSquareXTwitter size={iconSize.size}/></a></li>
+          <li><a href={details.instagramHandle} target="_blank" rel="noopener noreferrer"><FaInstagram size={iconSize.size}/></a></li>
+          <li><a href={details.youtubeLink} target="_blank" rel="noopener noreferrer"><FaYoutubeSquare size={iconSize.size}/></a></li>
+          <li><a href={details.tiktokLink} target="_blank" rel="noopener noreferrer"><AiFillTikTok size={iconSize.size}/></a></li>
           <li>{details.additionalLinks}</li>
         </ul>
       </div>
