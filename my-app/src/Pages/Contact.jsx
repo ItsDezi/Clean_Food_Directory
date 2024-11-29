bbae8f9d78ba4f24b970d4ffd77fa67d28bb5434
ItsDezi <j_desman@live.concordia.ca>
2024-11-29 02:20:10 -0500

    Basic contact form submitted but not connected

--- my-app/src/Pages/Contact.jsx
+++ my-app/src/Pages/Contact.jsx
@@ -1,5 +1,34 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { Form } from "react-bootstrap";
 
 function Contact () {
 
+    const handleSubmit = () => {
+        
+    }
+
+    return (
+        <div className="container form-container" >
+        <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Your name</Form.Label>
+          <Form.Control id='name' type="name" placeholder="John Doe" onChange={(e) => {}}/>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Your email</Form.Label>
+          <Form.Control id='email' type="email" placeholder="johnDoe@example.com" onChange={(e) => {}}/>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Your message</Form.Label>
+          <Form.Control id='message' type="textarea" as="textarea" placeholder="I've found an issue with this feature..." onChange={(e) => {}}/>
+        </Form.Group>
+
+        <Form.Control id='submit_btn' type="submit" />
+
+        </Form>
+        </div>
+    )
 }
 export default Contact;
