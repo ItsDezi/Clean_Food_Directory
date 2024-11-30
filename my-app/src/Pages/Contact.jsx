7e37a82759bc9d87773d9a745d502c3d76bc874d
ItsDezi <j_desman@live.concordia.ca>
2024-11-30 13:23:29 -0500

    added form validation for contact page

--- my-app/src/Pages/Contact.jsx
+++ my-app/src/Pages/Contact.jsx
@@ -5,6 +5,11 @@ import { useState } from 'react';
 import emailjs from "@emailjs/browser";
 function Contact () {
   const [loading, setLoading] = useState(false);
+  const [isValid, setIsValid] = useState(true);
+  const [ nameError, setNameError ] = useState();
+  const [ emailError, setEmailError ] = useState();
+  const [ messageError, setMessageError ] = useState();
+
   const [ formData, setFormData ] = useState({
       contact_name: "",
       contact_email: "",
@@ -14,6 +19,7 @@ function Contact () {
   const handleChange = (e) =>{
     //e.preventDefault();
     //console.log(e.target.value);
+    //validate();
     console.log(e);
     const attribute = e.target.id;
     console.log(formData);
@@ -47,7 +53,7 @@ function Contact () {
       `${process.env.REACT_APP_EMAIL_KEY}`,
     ).then(() => {
       setLoading(false);
-      alert("Thank you! I'll get back to you as soon as possible.");
+      alert("Thank you! We'll get back to you as soon as possible.");
       // setForm({
       //   name: '',
       //   email: '',
@@ -59,10 +65,42 @@ function Contact () {
       alert("An error occurred while sending the message. You can reach me at Juliend290@yahoo.com.");
     })
   }
+  const validate = () => {
+    if(!formData.contact_name)
+    {
+      setIsValid(false);
+      setNameError("Please enter a valid name.");
+      return false;
+    }
+    else if(!formData.contact_email || !formData.contact_email.includes("@"))
+    {
+      setIsValid(false);
+      setEmailError("Please enter a valid email.");
+      setNameError();
+      setMessageError();
+      return false;
+    }
+    else if(!formData.contact_message)
+      {
+        setIsValid(false);
+        setMessageError("Please enter a message.");
+        setNameError();
+        setEmailError();
+        return false;
+      }
+    setNameError();
+    setEmailError();
+    setMessageError();
+    setIsValid(true);
+    return true;
+  }
     const handleSubmit = async (event) => {
       event.preventDefault();
+        if(validate() === true)
+        {
         handleEmail();
         databaseUpload();
+        }
     }
 
     return (
@@ -70,17 +108,21 @@ function Contact () {
         <Form onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Label>Your name</Form.Label>
-          <Form.Control id='contact_name' type="name" placeholder="John Doe" onChange={(e) => {handleChange(e)}}/>
+          <Form.Control id='contact_name' type="name" placeholder="John Doe" onChange={(e) => {handleChange(e)}} style={{ border: !nameError ? '1px solid gray' : '2px solid red' }}/>
+          <Form.Text style={{ color: isValid ? 'white':'red'}}>{nameError}</Form.Text>
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Your email</Form.Label>
-          <Form.Control id='contact_email' type="email" placeholder="johnDoe@example.com" onChange={(e) => {handleChange(e)}}/>
+          <Form.Control id='contact_email' type="email" placeholder="johnDoe@example.com" onChange={(e) => {handleChange(e)}} style={{ border: !emailError ? '1px solid gray' : '2px solid red' }}/>
+          <Form.Text style={{ color: isValid ? 'white':'red'}}>{emailError}</Form.Text>
+
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Your message</Form.Label>
-          <Form.Control id='contact_message' type="textarea" as="textarea" placeholder="I've found an issue with this feature..." onChange={(e) => {handleChange(e)}}/>
+          <Form.Control id='contact_message' type="textarea" as="textarea" placeholder="I've found an issue with this feature..." onChange={(e) => {handleChange(e)}} style={{ border: !messageError ? '1px solid gray' : '2px solid red' }}/>
+          <Form.Text style={{ color: isValid ? 'white':'red'}}>{messageError}</Form.Text>
         </Form.Group>
 
         <Form.Control id='submit_btn' type="submit" />
